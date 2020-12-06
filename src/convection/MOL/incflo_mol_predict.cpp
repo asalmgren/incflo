08201d89f229271a63a3e5d212a4bf8e7b0549f5
Ann Almgren <asalmgren@lbl.gov>
2020-12-06 12:21:52 -0800

    remove print statements

--- src/convection/MOL/incflo_mol_predict.cpp
+++ src/convection/MOL/incflo_mol_predict.cpp
@@ -192,7 +192,6 @@ mol::predict_vels_on_faces (int lev,
             }
 
             u(i,j,k) = u_val;
-            amrex::Print() << "U " << IntVect(i,j) << " " << u(i,j,k) << std::endl;
         });
     }
 
@@ -269,7 +268,6 @@ mol::predict_vels_on_faces (int lev,
             }
 
             v(i,j,k) = v_val;
-            amrex::Print() << "V " << IntVect(i,j) << " " << v(i,j,k) << std::endl;
         });
     }
 
