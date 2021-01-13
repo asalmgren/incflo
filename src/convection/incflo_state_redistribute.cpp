667c82de7316564c46b2bcbfebbc392655d09c38
Ann Almgren <asalmgren@lbl.gov>
2021-01-13 10:28:15 -0800

    Keep git version more up to date ... state redistribution is still a work in progress

--- src/convection/incflo_state_redistribute.cpp
+++ src/convection/incflo_state_redistribute.cpp
@@ -8,88 +8,114 @@ void incflo::state_redistribute_eb (Box const& bx, int ncomp,
                                     Array4<Real const> const& dUdt_in,
                                     Array4<Real> const& scratch,
                                     Array4<EBCellFlag const> const& flag,
+                                    AMREX_D_DECL(Array4<Real const> const& apx,
+                                                 Array4<Real const> const& apy,
+                                                 Array4<Real const> const& apz),
                                     Array4<Real const> const& vfrac,
+                                    AMREX_D_DECL(Array4<Real const> const& fcx,
+                                                 Array4<Real const> const& fcy,
+                                                 Array4<Real const> const& fcz),
                                     Geometry& lev_geom)
 {
     const Box dbox = lev_geom.growPeriodicDomain(2);
 
-    Array4<Real> tmp(scratch, 0);
-    Array4<Real> delm(scratch, ncomp);
-    Array4<Real> wgt(scratch, 2*ncomp);
+    amrex::Print() << " DOING BOX " << bx << " with ncomp " << ncomp << std::endl;
 
     Box const& bxg1 = amrex::grow(bx,1);
     Box const& bxg2 = amrex::grow(bx,2);
 
-    // xxxxx TODO: more weight options
-    amrex::ParallelFor(bxg2,
+    IArrayBox nbor_fab(bx,9);
+    FArrayBox nbor_wt_fab(bx,1);
+    nbor_fab.setVal(0);
+    nbor_wt_fab.setVal(0.);
+    Array4<int>  nbor    = nbor_fab.array();
+    Array4<Real> nbor_wt = nbor_wt_fab.array();
+
+    amrex::ParallelFor(bx,
     [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
-        wgt(i,j,k) = (dbox.contains(IntVect(AMREX_D_DECL(i,j,k)))) ? 1.0 : 0.0;
-    });
+        if (flag(i,j,k).isSingleValued() and vfrac(i,j,k) < 0.5 && i > 3 && i < 5) 
+        {
+            amrex::Print() << "SMALL CELL " << IntVect(i,j) << " with vfrac " << vfrac(i,j,k) << std::endl;
 
-    amrex::ParallelFor(bxg1, ncomp,
-    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
-    {
-        if (flag(i,j,k).isSingleValued()) {
-            Real vtot = 0.0;
-            Real divnc = 0.0;
-            for (int kk = -1; kk <= 1; ++kk) {
-            for (int jj = -1; jj <= 1; ++jj) {
-            for (int ii = -1; ii <= 1; ++ii) {
-                 if ((ii != 0 or jj != 0 or kk != 0) and
-                     flag(i,j,k).isConnected(ii,jj,kk) and
-                    dbox.contains(IntVect(AMREX_D_DECL(i+ii,j+jj,k+kk))))
+            if (apx(i,j,k) > 0.)
+            {
+                if (fcx(i,j,k,0) <= 0.)
                 {
-                    Real vf = vfrac(i+ii,j+jj,k+kk);
-                    vtot += vf;
-                    divnc += vf * dUdt_in(i+ii,j+jj,k+kk,n);
+                    if (vfrac(i-1,j  ,k) > 0.) nbor(i,j,k,3) = 1;
+                    if (vfrac(i-1,j-1,k) > 0.) nbor(i,j,k,0) = 1;
+                    if (vfrac(i  ,j-1,k) > 0.) nbor(i,j,k,1) = 1;
+                } else {
+                    if (vfrac(i-1,j  ,k) > 0.) nbor(i,j,k,3) = 1;
+                    if (vfrac(i-1,j+1,k) > 0.) nbor(i,j,k,6) = 1;
+                    if (vfrac(i  ,j+1,k) > 0.) nbor(i,j,k,7) = 1;
                 }
-            }}}
-            divnc /= (vtot + 1.e-80);
-            Real optmp = (1.0-vfrac(i,j,k))*(divnc-dUdt_in(i,j,k,n));
-            tmp(i,j,k,n) = optmp;
-            delm(i,j,k,n) = -vfrac(i,j,k)*optmp;
-        } else {
-            tmp(i,j,k,n) = 0.0;
+            }
+
+            if (apx(i+1,j,k) > 0.)
+            {
+                if (fcx(i+1,j,k,0) <= 0.)
+                {
+                    if (vfrac(i+1,j  ,k) > 0.) nbor(i,j,k,5) = 1;
+                    if (vfrac(i+1,j-1,k) > 0.) nbor(i,j,k,2) = 1;
+                    if (vfrac(i  ,j-1,k) > 0.) nbor(i,j,k,1) = 1;
+                } else {
+                    if (vfrac(i+1,j  ,k) > 0.) nbor(i,j,k,5) = 1;
+                    if (vfrac(i+1,j+1,k) > 0.) nbor(i,j,k,8) = 1;
+                    if (vfrac(i  ,j+1,k) > 0.) nbor(i,j,k,7) = 1;
+                }
+            }
+
+            if (apy(i,j,k) > 0.)
+            {
+                if (fcy(i,j,k,0) <= 0.)
+                {
+                    if (vfrac(i-1,j  ,k) > 0.) nbor(i,j,k,3) = 1;
+                    if (vfrac(i-1,j-1,k) > 0.) nbor(i,j,k,0) = 1;
+                    if (vfrac(i  ,j-1,k) > 0.) nbor(i,j,k,1) = 1;
+                } else {
+                    if (vfrac(i+1,j  ,k) > 0.) nbor(i,j,k,5) = 1;
+                    if (vfrac(i+1,j-1,k) > 0.) nbor(i,j,k,2) = 1;
+                    if (vfrac(i  ,j-1,k) > 0.) nbor(i,j,k,1) = 1;
+                }
+            }
+
+            if (apy(i,j+1,k) > 0.)
+            {
+                if (fcy(i,j+1,k,0) <= 0.)
+                {
+                    if (vfrac(i-1,j  ,k) > 0.) nbor(i,j,k,3) = 1;
+                    if (vfrac(i-1,j+1,k) > 0.) nbor(i,j,k,6) = 1;
+                    if (vfrac(i  ,j+1,k) > 0.) nbor(i,j,k,7) = 1;
+                } else {
+                    if (vfrac(i+1,j  ,k) > 0.) nbor(i,j,k,5) = 1;
+                    if (vfrac(i+1,j+1,k) > 0.) nbor(i,j,k,8) = 1;
+                    if (vfrac(i  ,j+1,k) > 0.) nbor(i,j,k,7) = 1;
+                }
+            }
+
+            nbor_wt(i,j,k) = vfrac(i,j,k);
+
+            for (int jj = -1; jj <= 1; jj++)  
+            for (int ii = -1; ii <= 1; ii++)  
+            {
+                int index = (jj+1)*3 + (ii+1);
+                if (nbor(i,j,k,index) == 1)
+                {
+                    amrex::Print() << IntVect(ii,jj) << " is connected with vol " << vfrac(i+ii,j+jj,k) << std::endl;
+                    nbor_wt(i,j,k) += vfrac(i+ii,j+jj,k);
+                }
+            }
+            amrex::Print() << "VOL IN NBOR OF CELL " << IntVect(i,j) << " " << nbor_wt(i,j,k) << std::endl;
         }
     });
 
-    amrex::ParallelFor(bxg1 & dbox, ncomp,
-    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
-    {
-        if (flag(i,j,k).isSingleValued()) {
-            Real wtot = 0.0;
-            for (int kk = -1; kk <= 1; ++kk) {
-            for (int jj = -1; jj <= 1; ++jj) {
-            for (int ii = -1; ii <= 1; ++ii) {
-                if ((ii != 0 or jj != 0 or kk != 0) and
-                    flag(i,j,k).isConnected(ii,jj,kk))
-                {
-                    wtot += vfrac(i+ii,j+jj,k+kk) * wgt(i+ii,j+jj,k+kk);
-                }
-            }}}
-            wtot = 1.0/(wtot+1.e-80);
-
-            Real dtmp = delm(i,j,k,n) * wtot;
-            for (int kk = -1; kk <= 1; ++kk) {
-            for (int jj = -1; jj <= 1; ++jj) {
-            for (int ii = -1; ii <= 1; ++ii) {
-                if ((ii != 0 or jj != 0 or kk != 0) and
-                    bx.contains(IntVect(AMREX_D_DECL(i+ii,j+jj,k+kk))) and
-                    flag(i,j,k).isConnected(ii,jj,kk))
-                {
-                    Gpu::Atomic::Add(&tmp(i+ii,j+jj,k+kk,n), dtmp*wgt(i+ii,j+jj,k+kk));
-                }
-            }}}
-        }
-    });
+    exit(0);
 
     amrex::ParallelFor(bx, ncomp,
     [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
     {
-        // dUdt(i,j,k,n) = dUdt_in(i,j,k,n) + tmp(i,j,k,n);
-        // HACK HACK HACK 
-        dUdt(i,j,k,n) = dUdt_in(i,j,k,n);
+        dUdt(i,j,k,n) = dUdt_in(i,j,k,n) + tmp(i,j,k,n);
     });
 }
 #endif
